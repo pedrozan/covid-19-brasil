319d8851b8d66dd3a9680c7b6e50738919b3eee6
Pedro Henrique <pedro.schleder@gmail.com>
2020-04-01 20:26:07 -0300

    :bug: Reading evironment vars from heroku

--- api/utils/database_connection.py
+++ api/utils/database_connection.py
@@ -2,10 +2,9 @@ import urllib.parse
 
 from pymongo import MongoClient
 
-from api.config import MONGO_PWD, MONGO_USER
 
-username = urllib.parse.quote_plus(MONGO_USER)
-password = urllib.parse.quote_plus(MONGO_PWD)
+username = urllib.parse.quote_plus(os.environ['MONGO_USER'])
+password = urllib.parse.quote_plus(os.environ['MONGO_PWD'])
 conn = MongoClient(
     f"mongodb+srv://{username}:{password}@cluster0-rfxki.mongodb.net/test?retryWrites=true&w=majority"
 )
