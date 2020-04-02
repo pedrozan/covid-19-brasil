fe650c83ff6235994cc5d5489eeb0d92b3694c77
Pedro Henrique <pedro.schleder@gmail.com>
2020-04-01 21:20:45 -0300

    :pencil: Adding title and version to docs

--- api/main.py
+++ api/main.py
@@ -2,6 +2,10 @@ from fastapi import Depends, FastAPI, Header, HTTPException
 
 from .routers import covid_19
 
-app = FastAPI()
+app = FastAPI(
+    title = "Covid-19 in Brazil",
+    description = "An API with the oficial data from the Health Ministry",
+    version = "0.0.1"
+)
 
 app.include_router(covid_19.router)
